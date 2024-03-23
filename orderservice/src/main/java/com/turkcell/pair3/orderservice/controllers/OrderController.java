0fa9dec8fc97a34d1d8663d791b755485e80656c
emreOytun <emreoytun58@gmail.com>
2024-03-23 14:49:19 +0300

    order controller name fix

--- orderservice/src/main/java/com/turkcell/pair3/orderservice/controllers/OrderController.java
+++ orderservice/src/main/java/com/turkcell/pair3/orderservice/controllers/OrderController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/orders")
-public class OrderRepository {
+public class OrderController {
     @GetMapping
     public int getId() { return 10; }
 }
